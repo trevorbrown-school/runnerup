de636967836f8f4208721b862bc9ccb694a67a83
Trevor Brown <trevorbrown.dev@gmail.com>
2020-10-15 01:48:12 -0400

    Did some more styling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react'
 import '../css/Input.css';
-const Input: React.FunctionComponent = () => {
+
+interface InputProps {
+    placeholder?: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+    type?: string
+
+}
+
+const Input: React.FunctionComponent<InputProps> = ({placeholder, type, value, onChange}) => {
     const [focus, setFocus] = useState(false);
     return (
         <div className={`Input-wrapper ${(focus) ? "Input-focused" : ""}`}>
-            <input type="text" onBlur={() => setFocus(false)} onFocus={() => setFocus(true)} className="Input-primary"/>
+            <input
+                onChange={onChange}
+                value={value}
+                placeholder={placeholder}
+                type={type || "text"}
+                onBlur={() => setFocus(false)}
+                onFocus={() => setFocus(true)}
+                className="Input-primary" />
         </div>
     )
 }
--- src/components/LoginContainer.tsx
+++ src/components/LoginContainer.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from './Input';
 const LoginContainer: React.FunctionComponent = () => {
+    const [text, setText] = useState("");
     return (
         <div className="LoginContainer">
-             <Input />
+            {/* 
+            TODO:
+                Create some standard button styles
+                create a secondary, primary and warning button
+
+                Style login container
+            */}
+            <Input placeholder="Email" type="email" value={text} onChange={(e) => setText(e.target.value)}/>
+            <Input placeholder="Password" type="password" value={text} onChange={(e) => setText(e.target.value)}/>
         </div>
     )
 }
