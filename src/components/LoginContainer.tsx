07bc7d973db6559a02350d9b02c72ae7c292c2ee
Trevor Brown <trevorbrown.dev@gmail.com>
2020-10-15 19:32:12 -0400

    Finished login component

--- src/components/LoginContainer.tsx
+++ src/components/LoginContainer.tsx
@@ -9,19 +9,36 @@ import '../css/Button.css'
 //                 Style login container
 
 const LoginContainer: React.FunctionComponent = () => {
-    const [text, setText] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [remember, setRemember] = useState(false);
+
+    const renderEye = () => {
+        return <i className={`LoginContainer_eye fas fa-eye${(!passwordVisible) ? "-slash demphasis" : " emphasis"}`} onClick={() => setPasswordVisible(!passwordVisible)}></i>
+    }
+
+    const checkBox = () => {
+        return (
+            <span onClick={() => setRemember(!remember)} style={{
+                alignSelf: "flex-start", fontSize: "2rem", cursor: "pointer", userSelect: "none"}}>
+            <i id="remember-me" className={`${(!remember) ? "far" : "fas"} fa-check-square LoginContainer_checkbox`} ></i><label htmlFor="remember-me" style={{cursor: "pointer"}}>Remember Me</label>
+        </span>
+        );
+
+    }
+    
     return (
 
 
         <div className="LoginContainer">
-            <h1 style={{ fontFamily: "Sans-Serif, FontAwesome"}}>Login</h1>
+            <h1 style={{ fontFamily: "Sans-Serif, FontAwesome" }}>Login</h1>
 
             <div className="LoginContainer_flex u-space-around">
-                <Input placeholder="&#xF0E0;  Email" type="email" value={text} onChange={(e) => setText(e.target.value)} className="LoginContainer_input" />
-                <Input placeholder="&#xf023;  Password" type="password" value={text} onChange={(e) => setText(e.target.value)} className="LoginContainer_input" />
-                <span style={{alignSelf: "flex-start"}}>
-                <input type="checkbox" name="Remember Me" id="remember-me" /> <label htmlFor="remember-me">Remember Me</label>
-                </span>
+                <Input placeholder="&#xF0E0;  Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="LoginContainer_input" />
+                <div className="LoginContainer_input" style={{position: "relative"}}><Input placeholder="&#xf023;  Password" type={(passwordVisible) ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)}  />{renderEye()}</div>
+                
+                    {checkBox()}
                 <button className="Button" style={{ marginRight: "2rem", width: "100%" }}>Login</button>
             </div>
             <div className="LoginContainer_options">
